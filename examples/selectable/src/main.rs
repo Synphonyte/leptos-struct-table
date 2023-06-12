e4779af7bb4cfea7678867d6601f257518ce5892
joelbeicher <joel@synphonyte.com>
2023-06-12 19:39:01 +0200

    added selectable example with tailwind

---
+++ examples/selectable/src/main.rs
@@ -0,0 +1,70 @@
+mod tailwind;
+
+use async_trait::async_trait;
+use chrono::NaiveDate;
+use leptos::*;
+use leptos_struct_table::*;
+use serde::{Deserialize, Serialize};
+use tailwind::TailwindClassesPreset;
+
+// This generates the component BookTable
+#[derive(TableComponent, Serialize, Deserialize, Debug, Clone, Default, PartialEq)]
+#[table(sortable, selection_mode = "single", row_class = "select-none", classes_provider = "TailwindClassesPreset")]
+pub struct Book {
+    #[table(key)]
+    pub id: u32,
+    pub title: String,
+    pub author: String,
+    #[table(
+        cell_class = "text-red-600 dark:text-red-400",
+        head_class = "text-red-700 dark:text-red-300"
+    )]
+    pub publish_date: NaiveDate,
+}
+
+fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    mount_to_body(|cx| {
+        let items = create_rw_signal(
+            cx,
+            vec![
+                Book {
+                    id: 1,
+                    title: "The Great Gatsby".to_string(),
+                    author: "F. Scott Fitzgerald".to_string(),
+                    publish_date: NaiveDate::from_ymd_opt(1925, 4, 10).unwrap(),
+                },
+                Book {
+                    id: 2,
+                    title: "The Grapes of Wrath".to_string(),
+                    author: "John Steinbeck".to_string(),
+                    publish_date: NaiveDate::from_ymd_opt(1939, 4, 14).unwrap(),
+                },
+                Book {
+                    id: 3,
+                    title: "Nineteen Eighty-Four".to_string(),
+                    author: "George Orwell".to_string(),
+                    publish_date: NaiveDate::from_ymd_opt(1949, 6, 8).unwrap(),
+                },
+                Book {
+                    id: 4,
+                    title: "Ulysses".to_string(),
+                    author: "James Joyce".to_string(),
+                    publish_date: NaiveDate::from_ymd_opt(1922, 2, 2).unwrap(),
+                },
+            ],
+        );
+
+        let selected_key = create_rw_signal(cx, None);
+
+        create_effect(cx, move |_| log!("{:?}", selected_key.get()));
+
+        view! { cx,
+            <div class="rounded-md overflow-clip m-10 border dark:border-gray-700 float-left".to_string()>
+                <BookTable class="mb-[-1px]".to_string() items=items selected_key=selected_key/>
+            </div>
+        }
+    })
+}
---
+++ examples/selectable/src/tailwind.rs
@@ -0,0 +1,66 @@
+use crate::{ColumnSort, TableClassesProvider};
+
+#[derive(Clone, Copy)]
+pub struct TailwindClassesPreset;
+
+impl TableClassesProvider for TailwindClassesPreset {
+    fn new() -> Self {
+        Self
+    }
+
+    fn table(&self, classes: &str) -> String {
+        format!(
+            "{} {}",
+            "text-sm text-left text-gray-500 dark:text-gray-400", classes
+        )
+    }
+
+    fn head_row(&self, template_classes: &str) -> String {
+        format!(
+            "{} {}",
+            "text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-300",
+            template_classes
+        )
+    }
+
+    fn head_cell(&self, sort: ColumnSort, template_classes: &str) -> String {
+        let sort_class = match sort {
+            ColumnSort::None => "",
+            _ => "text-black dark:text-white",
+        };
+
+        format!(
+            "cursor-pointer px-5 py-2 {} {}",
+            sort_class, template_classes
+        )
+    }
+
+    fn head_cell_inner(&self) -> String {
+        "flex items-center after:content-[--sort-icon] after:pl-1 after:opacity-40 before:content-[--sort-priority] before:order-last before:pl-0.5 before:font-light before:opacity-40".to_string()
+    }
+
+    fn row(&self, row_index: usize, selected: bool, template_classes: &str) -> String {
+        let bg_color = if row_index % 2 == 0 {
+            if selected {
+                "bg-sky-300 text-gray-700 dark:bg-sky-700 dark:text-gray-400"
+            } else {
+                "bg-white dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800"
+            }
+        } else {
+            if selected {
+                "bg-sky-300 text-gray-700 dark:bg-sky-700 dark:text-gray-400"
+            } else {
+                "bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700"
+            }
+        };
+
+        format!(
+            "{} {} {}",
+            "border-b dark:border-gray-700", bg_color, template_classes
+        )
+    }
+
+    fn cell(&self, template_classes: &str) -> String {
+        format!("{} {}", "px-5 py-2", template_classes)
+    }
+}
