ff92dd0a889e42cebc553cf021c8d26fd2ef7ace
Maccesch <maccesch@synphonyte.com>
2024-06-04 16:03:51 +0200

    fixes #37

--- src/components/table_content.rs
+++ src/components/table_content.rs
@@ -412,7 +412,7 @@ where
         set_display_range.set(match display_strategy {
             DisplayStrategy::Virtualization | DisplayStrategy::InfiniteScroll => range.clone(),
             DisplayStrategy::Pagination { row_count, .. } => {
-                first_visible_row_index..first_visible_row_index + row_count.min(end)
+                first_visible_row_index..(first_visible_row_index + row_count).min(end)
             }
         });
 
