6041cf69a56c49131822791486d04931b5e71d55
Maccesch <maccesch@synphonyte.com>
2024-06-04 16:18:40 +0200

    reduced code duplication in chrono impl

--- src/chrono.rs
+++ src/chrono.rs
@@ -10,71 +10,70 @@ pub struct RenderChronoOptions {
     pub string: Option<String>,
 }
 
-/// Implementation for [`NaiveDate`] to work with the [`TableRow`] derive and the [`DefaultTableCellRenderer`]
-/// ```
-/// # use leptos_struct_table::*;
-/// # use leptos::*;
-/// # use ::chrono::NaiveDate;
-/// #[derive(TableRow, Clone)]
-/// #[table]
-/// struct SomeStruct {
-///     #[table(format(string = "%Y-%m-%d"))]
-///     my_field: NaiveDate
-/// }
-/// ```
-impl CellValue for NaiveDate {
-    type RenderOptions = RenderChronoOptions;
-    fn render_value(self, options: &Self::RenderOptions) -> impl IntoView {
-        if let Some(value) = options.string.as_ref() {
-            self.format(&value).to_string()
-        } else {
-            self.to_string()
+macro_rules! chrono_cell_value_impl {
+    (
+        $(#[$outer:meta])*
+        $ty:ty
+    ) => {
+        $(#[$outer])*
+        impl CellValue for $ty {
+            type RenderOptions = RenderChronoOptions;
+
+            fn render_value(self, options: &Self::RenderOptions) -> impl IntoView {
+                if let Some(value) = options.string.as_ref() {
+                    self.format(&value).to_string()
+                } else {
+                    self.to_string()
+                }
+            }
         }
-    }
+    };
 }
 
-/// Implementation for [`NaiveDateTime`] to work with the [`TableRow`] derive and the [`DefaultTableCellRenderer`]
-/// ```
-/// # use leptos_struct_table::*;
-/// # use leptos::*;
-/// # use ::chrono::NaiveDateTime;
-/// #[derive(TableRow, Clone)]
-/// #[table]
-/// struct SomeStruct {
-///     #[table(format(string = "%Y-%m-%d %H:%M:%S"))]
-///     my_field: NaiveDateTime
-/// }
-/// ```
-impl CellValue for NaiveDateTime {
-    type RenderOptions = RenderChronoOptions;
-    fn render_value(self, options: &Self::RenderOptions) -> impl IntoView {
-        if let Some(value) = options.string.as_ref() {
-            self.format(&value).to_string()
-        } else {
-            self.to_string()
-        }
-    }
-}
+chrono_cell_value_impl!(
+    /// Implementation for [`NaiveDate`] to work with the [`TableRow`] derive and the [`DefaultTableCellRenderer`]
+    /// ```
+    /// # use leptos_struct_table::*;
+    /// # use leptos::*;
+    /// # use ::chrono::NaiveDate;
+    /// #[derive(TableRow, Clone)]
+    /// #[table]
+    /// struct SomeStruct {
+    ///     #[table(format(string = "%Y-%m-%d"))]
+    ///     my_field: NaiveDate
+    /// }
+    /// ```
+    NaiveDate
+);
 
-/// Implementation for [`NaiveTime`] to work with the [`TableRow`] derive and the [`DefaultTableCellRenderer`]
-/// ```
-/// # use leptos_struct_table::*;
-/// # use leptos::*;
-/// # use ::chrono::NaiveTime;
-/// #[derive(TableRow, Clone)]
-/// #[table]
-/// struct SomeStruct {
-///     #[table(format(string = "%H:%M:%S"))]
-///     my_field: NaiveTime
-/// }
-/// ```
-impl CellValue for NaiveTime {
-    type RenderOptions = RenderChronoOptions;
-    fn render_value(self, options: &Self::RenderOptions) -> impl IntoView {
-        if let Some(value) = options.string.as_ref() {
-            self.format(&value).to_string()
-        } else {
-            self.to_string()
-        }
-    }
-}
+chrono_cell_value_impl!(
+    /// Implementation for [`NaiveDateTime`] to work with the [`TableRow`] derive and the [`DefaultTableCellRenderer`]
+    /// ```
+    /// # use leptos_struct_table::*;
+    /// # use leptos::*;
+    /// # use ::chrono::NaiveDateTime;
+    /// #[derive(TableRow, Clone)]
+    /// #[table]
+    /// struct SomeStruct {
+    ///     #[table(format(string = "%Y-%m-%d %H:%M:%S"))]
+    ///     my_field: NaiveDateTime
+    /// }
+    /// ```
+    NaiveDateTime
+);
+
+chrono_cell_value_impl!(
+    /// Implementation for [`NaiveTime`] to work with the [`TableRow`] derive and the [`DefaultTableCellRenderer`]
+    /// ```
+    /// # use leptos_struct_table::*;
+    /// # use leptos::*;
+    /// # use ::chrono::NaiveTime;
+    /// #[derive(TableRow, Clone)]
+    /// #[table]
+    /// struct SomeStruct {
+    ///     #[table(format(string = "%H:%M:%S"))]
+    ///     my_field: NaiveTime
+    /// }
+    /// ```
+    NaiveTime
+);
